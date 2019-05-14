d02e6e29e60b8e94f7debf47cea883ffedcae932
samussamu <44868248+samussamu@users.noreply.github.com>
2019-05-14 14:42:39 +0200

    Add files via upload

---
+++ Proyecto/src/model/conexion.java
@@ -0,0 +1,41 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class conexion {
+	
+	private static conexion cone;
+	private Connection connection;
+	private conexion() {
+		
+	}
+	
+	public static conexion getInstance() {
+		if(cone == null) {
+			cone= new conexion();
+		}
+		return cone;
+	}
+	
+	public Connection getConnection(){
+		connection = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            if(connection == null)
+                connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/m_Proyecto?useSSL=false&useLegacyDatetimeCode=false&serverTimezone=UTC","m","1234");
+ 
+        } catch (ClassNotFoundException e) {
+ 
+            e.printStackTrace();
+             
+        } catch (SQLException e) {
+             
+            e.printStackTrace();
+             
+        }
+        return connection;
+    }
+	
+}
--- Proyecto/src/model/jdbcLoginDAO.java
+++ Proyecto/src/model/jdbcLoginDAO.java
@@ -10,29 +10,14 @@ import dto.LoginDTO;
 
 public class jdbcLoginDAO  {
 
-private Connection connection;
+public void añadir() {
+	conexion.getInstance().getConnection().p
+	
+}
 	
 	
 	
-	
-	public Connection getConnection(){
-		connection = null;
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            if(connection == null)
-                connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/m_Proyecto?useSSL=false&useLegacyDatetimeCode=false&serverTimezone=UTC","m","1234");
- 
-        } catch (ClassNotFoundException e) {
- 
-            e.printStackTrace();
-             
-        } catch (SQLException e) {
-             
-            e.printStackTrace();
-             
-        }
-        return connection;
-    }
+
 
 	}
 		
